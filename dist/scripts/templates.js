838810c5a29f03e46f36e8f3847c4320d88e1a21
Samuel Padgett <spadgett@redhat.com>
2017-07-05 13:39:59 -0400

    Improve YAML editor validation and feedback
    - Create a common YAML editor component.
    - Show errors and warnings as Ace editor annotations by the line number.
    - For compatibility with `oc create`, accept YAML with duplicate keys
      when importing YAML. Duplicate keys when importing YAML are considered
      a warning.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -7312,6 +7312,7 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "<parse-error error=\"error\" ng-show=\"error\"></parse-error>\n" +
     "<div class=\"row\">\n" +
     "<div class=\"col-sm-12 pod-bottom-xl\">\n" +
+<<<<<<< 4c90bbeb06cb1c3b6110ce79a149d15b4a595d99
     "<form name=\"form\">\n" +
     "<div class=\"form-group\" id=\"from-file\">\n" +
     "<osc-file-input model=\"editorContent\" drop-zone-id=\"from-file\" help-text=\"{{'Upload file by dragging & dropping, selecting it, or pasting from the clipboard.'|translate}}\" ng-disabled=\"false\"></osc-file-input>\n" +
@@ -7328,6 +7329,12 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "</div>\n" +
     "<div ng-if=\"!isDialog\" class=\"buttons gutter-bottom\">\n" +
     "<button type=\"submit\" ng-click=\"create()\" ng-disabled=\"editorErrorAnnotation || !editorContent\" class=\"btn btn-primary btn-lg\" translate>\n" +
+=======
+    "<form name=\"form\" id=\"from-file\">\n" +
+    "<ui-ace-yaml resource=\"resource\" ng-required=\"true\" show-file-input=\"true\"></ui-ace-yaml>\n" +
+    "<div ng-if=\"!isDialog\" class=\"buttons gutter-bottom\">\n" +
+    "<button type=\"submit\" ng-click=\"create()\" ng-disabled=\"form.$invalid\" class=\"btn btn-primary btn-lg\">\n" +
+>>>>>>> Improve YAML editor validation and feedback
     "Create\n" +
     "</button>\n" +
     "<a class=\"btn btn-default btn-lg\" href=\"\" role=\"button\" ng-click=\"cancel()\" translate>\n" +
@@ -8111,7 +8118,11 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "</div>\n" +
     "<textarea class=\"form-control\" rows=\"5\" ng-show=\"showTextArea || !supportsFileUpload\" ng-model=\"model\" ng-required=\"required\" ng-disabled=\"disabled\" autocorrect=\"off\" autocapitalize=\"none\" spellcheck=\"false\" ng-attr-aria-describedby=\"{{helpText ? helpID : undefined}}\">\n" +
     "  </textarea>\n" +
+<<<<<<< 4c90bbeb06cb1c3b6110ce79a149d15b4a595d99
     "<a href=\"\" ng-show=\"(model || fileName) && !disabled\" ng-click=\"cleanInputValues()\" translate>Clear Value</a>\n" +
+=======
+    "<a href=\"\" ng-show=\"(model || fileName) && !disabled && !hideClear\" ng-click=\"cleanInputValues()\">Clear Value</a>\n" +
+>>>>>>> Improve YAML editor validation and feedback
     "</div>"
   );
 
@@ -9248,6 +9259,39 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "</tr>\n" +
     "</tbody>\n" +
     "</table>"
+  );
+
+
+  $templateCache.put('views/directives/ui-ace-yaml.html',
+    "<ng-form name=\"$ctrl.form\">\n" +
+    "<div class=\"form-group\" id=\"yaml-file\">\n" +
+    "<osc-file-input ng-if=\"$ctrl.showFileInput\" model=\"$ctrl.fileUpload\" drop-zone-id=\"yaml-file\" help-text=\"Upload a file by dragging & dropping, selecting it, or pasting from the clipboard.\" ng-disabled=\"false\" hide-clear=\"true\"></osc-file-input>\n" +
+    "<div class=\"edit-yaml-errors\">\n" +
+    "\n" +
+    "<div ng-if=\"firstError = $ctrl.annotations.error[0]\">\n" +
+    "<a href=\"\" ng-click=\"$ctrl.gotoLine(firstError.row)\">\n" +
+    "<span class=\"pficon pficon-error-circle-o\" aria-hidden=\"true\"></span>\n" +
+    "Error\n" +
+    "</a>\n" +
+    "</div>\n" +
+    "<div ng-if=\"firstWarning = $ctrl.annotations.warning[0]\">\n" +
+    "<a href=\"\" ng-click=\"$ctrl.gotoLine(firstWarning.row)\">\n" +
+    "<span class=\"pficon pficon-warning-triangle-o\" aria-hidden=\"true\"></span>\n" +
+    "Warning\n" +
+    "</a>\n" +
+    "</div>\n" +
+    "</div>\n" +
+    "<div ui-ace=\"{\n" +
+    "      mode: 'yaml',\n" +
+    "      theme: 'eclipse',\n" +
+    "      onLoad: $ctrl.aceLoaded,\n" +
+    "      onChange: $ctrl.aceChanged,\n" +
+    "      rendererOptions: {\n" +
+    "        showPrintMargin: false\n" +
+    "      }\n" +
+    "    }\" ng-model=\"$ctrl.model\" class=\"editor ace-bordered yaml-mode\" ng-required=\"$ctrl.ngRequired\" id=\"ace-yaml-editor\"></div>\n" +
+    "</div>\n" +
+    "</ng-form>"
   );
 
 
@@ -10396,6 +10440,7 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "<div class=\"container surface-shaded\">\n" +
     "<breadcrumbs breadcrumbs=\"breadcrumbs\"></breadcrumbs>\n" +
     "<alerts alerts=\"alerts\"></alerts>\n" +
+<<<<<<< 4c90bbeb06cb1c3b6110ce79a149d15b4a595d99
     "<div ng-if=\"!resource\" class=\"pad-top-md\" translate>Loading...</div>\n" +
     "<div ng-if=\"resource\" class=\"pad-top-md\">\n" +
     "<h1 class=\"truncate\"><translate>Edit</translate> <span class=\"hidden-xs\">{{resource.kind | humanizeKind : true | translate}}</span> {{resource.metadata.name}}</h1>\n" +
@@ -10410,21 +10455,35 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "<span class=\"pficon pficon-warning-triangle-o\" aria-hidden=\"true\"></span>\n" +
     "<span class=\"sr-only\" translate>Warning:</span>\n" +
     "<translate>{{resource.kind | humanizeKind | upperFirst}} <strong>{{resource.metadata.name}}</strong> has been deleted since you started editing it.</translate>\n" +
+=======
+    "<div ng-if=\"!updated.resource\" class=\"pad-top-md\">Loading...</div>\n" +
+    "<div ng-if=\"updated.resource\" class=\"pad-top-md\">\n" +
+    "<h1 class=\"truncate\">Edit <span class=\"hidden-xs\">{{updated.resource.kind | humanizeKind : true}}</span> {{updated.resource.metadata.name}}</h1>\n" +
+    "<parse-error error=\"error\" ng-if=\"error\"></parse-error>\n" +
+    "<div ng-if=\"resourceChanged && !resourceDeleted && !updatingNow\" class=\"alert alert-warning\">\n" +
+    "<span class=\"pficon pficon-warning-triangle-o\" aria-hidden=\"true\"></span>\n" +
+    "<span class=\"sr-only\">Warning:</span>\n" +
+    "{{updated.resource.kind | humanizeKind | upperFirst}} <strong>{{updated.resource.metadata.name}}</strong> has changed since you started editing it. You'll need to copy any changes you've made and edit the {{updated.resource.kind | humanizeKind}} again.\n" +
+    "</div>\n" +
+    "<div ng-if=\"resourceDeleted\" class=\"alert alert-warning\">\n" +
+    "<span class=\"pficon pficon-warning-triangle-o\" aria-hidden=\"true\"></span>\n" +
+    "<span class=\"sr-only\">Warning:</span>\n" +
+    "{{updated.resource.kind | humanizeKind | upperFirst}} <strong>{{updated.resource.metadata.name}}</strong> has been deleted since you started editing it.\n" +
+>>>>>>> Improve YAML editor validation and feedback
     "</div>\n" +
     "<confirm-on-exit dirty=\"modified\"></confirm-on-exit>\n" +
-    "\n" +
-    "<div ui-ace=\"{\n" +
-    "                mode: 'yaml',\n" +
-    "                theme: 'eclipse',\n" +
-    "                onLoad: aceLoaded,\n" +
-    "                rendererOptions: {\n" +
-    "                  showPrintMargin: false\n" +
-    "                }\n" +
-    "              }\" ng-model=\"editor.model\" class=\"editor ace-bordered yaml-mode\"></div>\n" +
+    "<form name=\"editor.form\">\n" +
+    "<ui-ace-yaml resource=\"updated.resource\" ng-required=\"true\"></ui-ace-yaml>\n" +
     "<div class=\"button-group mar-top-xl\">\n" +
+<<<<<<< 4c90bbeb06cb1c3b6110ce79a149d15b4a595d99
     "<button class=\"btn btn-lg btn-primary\" type=\"button\" ng-click=\"save()\" ng-disabled=\"!modified || resourceChanged || resourceDeleted || updatingNow\" translate>Save</button>\n" +
     "<button class=\"btn btn-lg btn-default\" type=\"button\" ng-disabled=\"updatingNow\" ng-click=\"cancel()\" translate>Cancel</button>\n" +
+=======
+    "<button class=\"btn btn-lg btn-primary\" type=\"button\" ng-click=\"save()\" ng-disabled=\"editor.form.$pristine || editor.form.$invalid || resourceChanged || resourceDeleted || updatingNow\">Save</button>\n" +
+    "<button class=\"btn btn-lg btn-default\" type=\"button\" ng-disabled=\"updatingNow\" ng-click=\"cancel()\">Cancel</button>\n" +
+>>>>>>> Improve YAML editor validation and feedback
     "</div>\n" +
+    "</form>\n" +
     "</div>\n" +
     "</div>\n" +
     "</div>\n" +
