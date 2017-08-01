2db7cac47de931269c160b3ad4d6b274c221e6a5
Robb Hamilton <rhamilto@redhat.com>
2017-08-01 10:43:00 -0400

    Changing in progress pipeline stages to use timeOnlyDurationUntilNow on stage.startTimeMillis since stage.durationMillis will only update every 30s
    Fixes #1891

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -5752,7 +5752,11 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "<div class=\"pipeline-actions\" ng-if=\"stage | pipelineStagePendingInput\">\n" +
     "<a ng-href=\"{{build | jenkinsInputURL}}\" target=\"_blank\" translate>Input Required</a>\n" +
     "</div>\n" +
+<<<<<<< b353967164da954a16b14da15ba317da6b2e869f
     "<div class=\"pipeline-time\" ng-class=\"stage.status\" ng-if=\"!stage.durationMillis && !(stage | pipelineStagePendingInput)\" translate>not started</div>\n" +
+=======
+    "<div class=\"pipeline-time\" ng-class=\"stage.status\" ng-if=\"!stage.durationMillis && !(stage | pipelineStagePendingInput)\">not started</div>\n" +
+>>>>>>> Changing in progress pipeline stages to use timeOnlyDurationUntilNow on stage.startTimeMillis since stage.durationMillis will only update every 30s
     "<div class=\"pipeline-time\" ng-class=\"stage.status\" ng-if=\"stage.startTimeMillis && !(stage | pipelineStagePendingInput) && !(stage | pipelineStageComplete)\"><time-only-duration-until-now timestamp=\"stage.startTimeMillis\"></time-only-duration-until-now></div>\n" +
     "<div class=\"pipeline-time\" ng-class=\"stage.status\" ng-if=\"stage.durationMillis && (stage | pipelineStageComplete)\">{{stage.durationMillis | timeOnlyDuration}}</div>\n" +
     "</div>\n" +
