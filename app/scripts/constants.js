acc73debc76d37f62295f9b6f20682fd276a4aff
Samuel Padgett <spadgett@redhat.com>
2017-09-06 12:28:58 -0400

    Adjust notification drawer events to show
    - Show events when pods fail to schedule
    - Show an event for invalid environment variable names
    - Remove ErrorAsyncOperationInProgress and ReferencesNonexistentInstance
      because they are expected and generate a lot of noise with in-context bind.

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -164,6 +164,7 @@ angular.extend(window.OPENSHIFT_CONSTANTS, {
     // General events that apply to more than one api object
     FailedCreate: true,
     FailedDelete: true,
+    FailedScheduling: true,
     FailedUpdate: true,
     // Build
     BuildCancelled: true,
@@ -175,7 +176,6 @@ angular.extend(window.OPENSHIFT_CONSTANTS, {
     // Deployment
     DeploymentCancelled: true,
     Failed: true,
-    ScalingReplicaSet: true,
     // DeploymentConfig
     DeploymentCreated: true,
     DeploymentCreationFailed: true,
@@ -185,6 +185,7 @@ angular.extend(window.OPENSHIFT_CONSTANTS, {
     // Pod
     BackOff: true,
     FailedSync: true,
+    InvalidEnvironmentVariableNames: true,
     Unhealthy: true,
     // PVC
     FailedBinding: true,
@@ -194,12 +195,9 @@ angular.extend(window.OPENSHIFT_CONSTANTS, {
     LoadBalancerUpdateFailed: true,
     // Service Catalog
     Deprovisioning: true,
-    ErrorAsyncOperationInProgress: true,
     ErrorCallingProvision: true,
     ErrorInjectingBindResult: true,
-    ProvisionedSuccessfully: true,
     Provisioning: true,
-    ReferencesNonexistentInstance: true,
     ReferencesNonexistentServiceClass: true,
     ReferencesNonexistentServicePlan: true,
     UnbindCallFailed: true
