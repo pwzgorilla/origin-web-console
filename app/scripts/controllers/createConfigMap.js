82b4cbd60dadca34cc3bbe0c086220de1182b855
benjaminapetersen <ben@benjaminapetersen.me>
2017-09-25 11:55:02 -0400

    Update createConfigMap controller to use getPreferredVersion

--- app/scripts/controllers/createConfigMap.js
+++ app/scripts/controllers/createConfigMap.js
@@ -32,6 +32,8 @@ angular.module('openshiftConsole')
       }
     ];
 
+    var configMapsVersion = APIService.getPreferredVersion('configmaps');
+
     var hideErrorNotifications = function() {
       NotificationsService.hideNotification("create-config-map-error");
     };
@@ -65,8 +67,7 @@ angular.module('openshiftConsole')
           if ($scope.createConfigMapForm.$valid) {
             hideErrorNotifications();
             $scope.disableInputs = true;
-            var createConfigMapVersion = APIService.objectToResourceGroupVersion($scope.configMap);
-            DataService.create(createConfigMapVersion, null, $scope.configMap, context)
+            DataService.create(configMapsVersion, null, $scope.configMap, context)
               .then(function() { // Success
                 NotificationsService.addNotification({
                   type: "success",
