ed7141d71afeea314ad6d1183c760dedc6ee73fe
sg00dwin <sgoodwin@redhat.com>
2017-10-16 11:01:22 -0400

    Use existing event class to set padding top and left Fixes https://github.com/openshift/origin-web-console/issues/2258

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -8340,7 +8340,11 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "<ellipsis-pulser color=\"dark\" size=\"sm\" msg=\"Loading\" ng-if=\"!events\" class=\"events\"></ellipsis-pulser>\n" +
     "<div ng-if=\"events\" class=\"events\">\n" +
     "<div ng-if=\"!(events | hashSize)\" class=\"event\">\n" +
+<<<<<<< eed768a10672b1ce9565159fa823c44f92a8d3b1
     "<em translate>No events.</em>\n" +
+=======
+    "<em>No events.</em>\n" +
+>>>>>>> Use existing event class to set padding top and left
     "</div>\n" +
     "<div ng-repeat=\"event in events track by (event | uid)\" class=\"event animate-repeat\" ng-class=\"{'highlight': highlightedEvents[event.involvedObject.kind + '/' + event.involvedObject.name]}\">\n" +
     "<span class=\"sr-only\">{{event.type}}</span>\n" +
