7805d1f04a3652c674cffed28e1a923f61d88a08
Samuel Padgett <spadgett@redhat.com>
2017-10-19 13:35:21 -0400

    Don't set propagationPolicy when deleting bindings

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -6016,7 +6016,11 @@ namespace: t.metadata.namespace
 }, l = a.getPreferredVersion("servicebindings");
 p(t, n).then(function(t) {
 _.each(t, function(t) {
-t.metadata.deletionTimestamp || i.delete(l, t.metadata.name, r).then(function() {
+if (!t.metadata.deletionTimestamp) {
+var n = {
+propagationPolicy: null
+};
+i.delete(l, t.metadata.name, r, n).then(function() {
 c.addNotification({
 type: "success",
 message: "Binding " + t.metadata.name + "' was marked for deletion."
@@ -6028,6 +6032,7 @@ message: "Binding " + t.metadata.name + "' could not be deleted.",
 details: e("getErrorDetails")(n)
 }), s.error("Binding " + t.metadata.name + "' could not be deleted.", n);
 });
+}
 });
 });
 }
