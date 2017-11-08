d10fd31317c417721b32d2f4a7861777e07091a1
Samuel Padgett <spadgett@redhat.com>
2017-11-08 09:35:11 -0500

    Add ProvisionedSuccessfully even to notification drawer

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -272,6 +272,7 @@ angular.extend(window.OPENSHIFT_CONSTANTS, {
     ErrorCallingProvision: true,
     ErrorInjectingBindResult: true,
     ProvisionCallFailed: true,
+    ProvisionedSuccessfully: true,
     Provisioning: true,
     ReferencesNonexistentServiceClass: true,
     ReferencesNonexistentServicePlan: true,
